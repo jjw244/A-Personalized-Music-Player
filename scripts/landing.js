d5028319276e346b9d99f23b81876ddadf9abb87
Jennifer washburn <jjw244@utexas.edu>
2016-12-15 18:56:44 -0600

    checkpiont 9 - Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
-var animatePoints = function () {
- 
-    var points = document.getElementsByClassName('point');
-    
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function (points) {
+    //debugger;
     var revealPoint = function (index) {
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
@@ -10,6 +10,23 @@ var animatePoints = function () {
     };
     
     for (var i = 0; i < points.length; i++) {
-        revealPoint();
+        revealPoint(i);
     };
 };
+
+window.onload = function() {
+    
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+     window.addEventListener('scroll', function(event) { 
+         //console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
