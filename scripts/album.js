40124cdb8ede14eeecd94c121ba24ec778983bb6
Jennifer washburn <jjw244@utexas.edu>
2016-12-18 14:38:11 -0600

    checkpoint 13 - Complete

--- scripts/album.js
+++ scripts/album.js
@@ -42,33 +42,86 @@ var createSongRow = function (songNumber, songName, songLength) {
     };
 
 var setCurrentAlbum = function (album) {
-     // #1
+     // Select all of the HTML elements required to display on the album page: title, artist, release info, image, and song list.
         var albumTitle = document.getElementsByClassName('album-view-title')[0];
         var albumArtist = document.getElementsByClassName('album-view-artist')[0];
         var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
         var albumImage = document.getElementsByClassName('album-cover-art')[0];
         var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
-     // #2
+     // firstChild property identifies the first child node of an element, and  nodeValue returns or sets the value of a node.
         albumTitle.firstChild.nodeValue = album.title;
         albumArtist.firstChild.nodeValue = album.artist;
         albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
         albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
+     // Clear the album song list HTML to make sure there are no interfering elements.
         albumSongList.innerHTML = '';
  
-     // #4
+     // Go through all the songs from the specified album object and insert them into the HTML using the innerHTML property. The createSongRow function is called at each loop, passing in the song number, name, and length arguments from our album object.
         for (var i = 0; i < album.songs.length; i++) {
             albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
         }
  };
- 
+
+var findParentByClassName = function(element, targetClass) {
+    
+    if (element) {
+        var currentParent = element.parentElement;
+        while (currentParent.className != targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        }
+        return currentParent;
+    }
+};
+
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
+
+var clickHandler = function(targetElement) {
+    
+    var songItem = getSongItem(targetElement);  
+    
+    if (currentlyPlayingSong === null) {
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+         songItem.innerHTML = playButtonTemplate;
+         currentlyPlayingSong = null;
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+
+ };
+
+ // Elements to which we'll be adding listeners
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
  // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+ // Store state of playing songs
+var currentlyPlayingSong = null;
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
@@ -76,14 +129,34 @@ window.onload = function() {
     songListContainer.addEventListener('mouseover', function(event) {
         // Only target individual song rows during event delegation
          if (event.target.parentElement.className === 'album-view-song-item') {
-              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+             var songItem = getSongItem(event.target);
+
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;            
+            }
          }
      });
     
     for (var i = 0; i < songRows.length; i++) {
         songRows[i].addEventListener('mouseleave', function(event) {
-            // Selects first child element, which is the song-item-number element
-             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            // Cached song item in a variable
+             var songItem = getSongItem(event.target);
+             var songItemNumber = songItem.getAttribute('data-song-number');
+ 
+             // Added the conditional that checks that the item the mouse is leaving is not the current song, and we only change the content if it isn't.
+             if (songItemNumber !== currentlyPlayingSong) {
+                 //songItem.innerHTML = songItemNumber;
+                 var songItem = getSongItem(event.target);
+
+                if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                    songItem.innerHTML = playButtonTemplate;
+                }
+             }
         });
+        
+        songRows[i].addEventListener('click', function(event) {
+             // Event handler call
+            clickHandler(event.target);
+         });
     }
 }
