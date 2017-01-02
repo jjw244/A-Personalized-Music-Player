c8c121106a89a55a2f279a08a97e5e5f33c63636
Jennifer washburn <jjw244@utexas.edu>
2017-01-02 14:05:12 -0600

    Checkpiont 13- Assignment - Complete

--- scripts/album.js
+++ scripts/album.js
@@ -66,13 +66,16 @@ var setCurrentAlbum = function (album) {
 
 var findParentByClassName = function(element, targetClass) {
     
-    if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
-        }
-        return currentParent;
+    var currentParent = element.parentElement;
+    
+    if (element.parentElement !== null) {        
+        alert("No parent found");
     }
+    if (currentParent.className !== targetClass) {
+        alert("No parent found with that class name");
+    } 
+    
+    return currentParent;
 };
 
 var getSongItem = function(element) {
