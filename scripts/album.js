cfec0fcc59184a60183e33d3ea2ae7dad30753a1
Jennifer washburn <jjw244@utexas.edu>
2017-01-03 18:55:10 -0600

    Checkpoint 17 - Complete

--- scripts/album.js
+++ scripts/album.js
@@ -36,31 +36,33 @@ var createSongRow = function (songNumber, songName, songLength) {
             + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
             + '  <td class="song-item-title">' + songName + '</td>'
             + '  <td class="song-item-duration">' + songLength + '</td>'
-            + '</tr>';
+            + '</tr>'
+            ;
  
-        return template;
+        return $(template);
     };
 
 var setCurrentAlbum = function (album) {
      // Select all of the HTML elements required to display on the album page: title, artist, release info, image, and song list.
-        var albumTitle = document.getElementsByClassName('album-view-title')[0];
-        var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-        var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-        var albumImage = document.getElementsByClassName('album-cover-art')[0];
-        var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+     var $albumTitle = $('.album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
  
      // firstChild property identifies the first child node of an element, and  nodeValue returns or sets the value of a node.
-        albumTitle.firstChild.nodeValue = album.title;
-        albumArtist.firstChild.nodeValue = album.artist;
-        albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-        albumImage.setAttribute('src', album.albumArtUrl);
+     $albumTitle.text(album.title);
+     $albumArtist.text(album.artist);
+     $albumReleaseInfo.text(album.year + ' ' + album.label);
+     $albumImage.attr('src', album.albumArtUrl);
  
      // Clear the album song list HTML to make sure there are no interfering elements.
-        albumSongList.innerHTML = '';
+        $albumSongList.empty();
  
      // Go through all the songs from the specified album object and insert them into the HTML using the innerHTML property. The createSongRow function is called at each loop, passing in the song number, name, and length arguments from our album object.
         for (var i = 0; i < album.songs.length; i++) {
-            albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+            var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+            $albumSongList.append($newRow);
         }
  };
 
