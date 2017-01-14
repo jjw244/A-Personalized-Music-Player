3a888c76741a18a69909b83b65bb60555f6a2bcd
Jennifer washburn <jjw244@utexas.edu>
2017-01-14 14:55:56 -0600

    Checkpoint 21 - Complete

--- scripts/album.js
+++ scripts/album.js
@@ -16,7 +16,13 @@ var setSong = function(songNumber) {
     
     setVolume(currentVolume);
 };
- 
+
+var seek = function(time) {
+     if (currentSoundFile) {
+         currentSoundFile.setTime(time);
+     }
+}
+
 var setVolume = function(volume) {
     if (currentSoundFile) {
          currentSoundFile.setVolume(volume);
@@ -52,12 +58,19 @@ var createSongRow = function (songNumber, songName, songLength) {
 
             if (currentlyPlayingSongNumber !== songNumber) {
                  // Switch from Play -> Pause button to indicate new song is playing.
-                $(this).html(pauseButtonTemplate);
                 setSong(songNumber);
                 currentSoundFile.play();
-                $(this).html(pauseButtonTemplate);
+                updateSeekBarWhileSongPlays();
                 currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+                
+                var $volumeFill = $('.volume .fill');
+                var $volumeThumb = $('.volume .thumb');
+                $volumeFill.width(currentVolume + '%');
+                $volumeThumb.css({left: currentVolume + '%'});
+                
+                $(this).html(pauseButtonTemplate);
                 updatePlayerBarSong();
+                
             } else if (currentlyPlayingSongNumber === songNumber) {
                 if (currentSoundFile.isPaused()) {
                     $(this).html(pauseButtonTemplate);
@@ -118,6 +131,78 @@ var setCurrentAlbum = function (album) {
         var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
         $albumSongList.append($newRow);
     }
+ };
+
+var updateSeekBarWhileSongPlays = function() {
+     if (currentSoundFile) {
+         // #10 bind() the timeupdate event to currentSoundFile
+         currentSoundFile.bind('timeupdate', function(event) {
+             // #11 use a new method for calculating the seekBarFillRatio
+             var seekBarFillRatio = this.getTime() / this.getDuration();
+             var $seekBar = $('.seek-control .seek-bar');
+ 
+             updateSeekPercentage($seekBar, seekBarFillRatio);
+         });
+     }
+};
+
+var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+    var offsetXPercent = seekBarFillRatio * 100;
+    // #1
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(100, offsetXPercent);
+ 
+    // #2
+    var percentageString = offsetXPercent + '%';
+    $seekBar.find('.fill').width(percentageString);
+    $seekBar.find('.thumb').css({left: percentageString});
+};
+
+var setupSeekBars = function() {
+    // #6 using jQuery to find all elements in the DOM with a class of "seek-bar" that are contained within the element with a class of "player-bar"
+     var $seekBars = $('.player-bar .seek-bar');
+ 
+     $seekBars.click(function(event) {
+         // #3
+         var offsetX = event.pageX - $(this).offset().left;
+         var barWidth = $(this).width();
+         // #4
+         var seekBarFillRatio = offsetX / barWidth;
+         
+         if ($(this).parent().attr('class') == 'seek-control') {
+            seek(seekBarFillRatio * currentSoundFile.getDuration());
+        } else {
+            setVolume(seekBarFillRatio * 100);   
+        }
+         // #5
+         updateSeekPercentage($(this), seekBarFillRatio);
+     });         
+         // #7 find elements with a class of .thumb inside our $seekBars and add an event listener for the mousedown event
+      $seekBars.find('.thumb').mousedown(function(event) {
+         // #8 taking the context of the event and wrapping it in jQuery
+         var $seekBar = $(this).parent();
+ 
+         // #9 introduces a new way to track events, jQuery's bind() event
+        $(document).bind('mousemove.thumb', function(event){
+             var offsetX = event.pageX - $seekBar.offset().left;
+             var barWidth = $seekBar.width();
+             var seekBarFillRatio = offsetX / barWidth;
+ 
+             if ($seekBar.parent().attr('class') == 'seek-control') {
+                seek(seekBarFillRatio * currentSoundFile.getDuration());   
+             } else {
+                setVolume(seekBarFillRatio);
+             }
+             
+             updateSeekPercentage($seekBar, seekBarFillRatio);
+        });
+         
+         // #10 bind the mouseup event with a .thumb namespace
+         $(document).bind('mouseup.thumb', function() {
+             $(document).unbind('mousemove.thumb');
+             $(document).unbind('mouseup.thumb');
+         });
+     });
  };
 
 
@@ -219,6 +304,7 @@ var $nextButton = $('.main-controls .next');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
+    setupSeekBars();
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
 });
--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -6,14 +6,13 @@ var albumPicasso = {
         year: '1881',
         albumArtUrl: 'assets/images/album_covers/01.png',
         songs: [
-            { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
-            { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
-            { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
-            { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink' },
-            { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta' }  
-
-        ]
-    };
+            { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+    ]
+};
  
  // Another Example Album
 var albumMarconi = {
@@ -29,4 +28,4 @@ var albumMarconi = {
             { title: 'Can you hear me now?', duration: '3:14' },
             { title: 'Wrong phone number', duration: '2:15'}
         ]
-    };
+};
